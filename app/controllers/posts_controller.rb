69c7c416f956f2b69139b30dd53c619611e3c55b
Kikuchi Shigeo <K@sigepon.local>
2016-07-07 19:07:10 +0900

    レイアウト・デザインを整える

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
   def index
+    # @posts = Post.all
+    @posts = Post.order(created_at: :desc)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+      params.require(:post).permit(:body)
   end
 end
